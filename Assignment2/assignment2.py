bb2296cc2abee22aa2ea50b6ebff2842f5672e15
BilalAkhtar88 <bilal_akhtar88@yahoo.com>
2018-10-02 18:07:50 +0200

    provided codes

--- Assignment2/assignment2.py
+++ Assignment2/assignment2.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 # Task 01, Defining Kernel Function
 def kernelFun(x1, x2, kernelType, p=1, sigma=2):
-    a = numpy.array(x1)
-    b = numpy.array(x2)
+    #   a = numpy.array(x1)
+    #   b = numpy.array(x2)
     if kernelType == 1:
         kernelOutput = numpy.dot(a,b)
     elif kernelType == 2:
@@ -19,7 +19,8 @@ def kernelFun(x1, x2, kernelType, p=1, sigma=2):
 # Task 02, Defining Objective Function
 def objectiveFun (alphaVec):
 #    alpha = numpy.array(alphaVec)
-    N = len(alpha)
+#    N = len(alpha)
+    global N
     sumAlpha = 0
     dualForm = 0
     while i < N:
@@ -34,8 +35,50 @@ def objectiveFun (alphaVec):
 # Task 05, Extracting non-zero alpha values
 # Task 06, Calculating b values
 # Task 07, Implementing Indicator Function
+
 # Task 08, Generating Test Data (Provided)
+
+numpy.random.seed(100)  #For getting same random numbers everytime
+
+classA = numpy.concatenate (
+(numpy.random.randn(10,2)*0.2 + [1.5,0.5] ,
+numpy.random.randn (10,2)*0.2 + [ -1.5,0.5]))
+
+classB = numpy.random.randn (20,2)*0.2 + [ 0.0 , -0.5]
+
+inputs = numpy.concatenate((classA ,classB))
+targets = numpy.concatenate (
+(numpy.ones(classA.shape[ 0 ]),
+-numpy.ones(classB.shape[0])))
+
+N = inputs.shape[0] # Number of rows ( samples )
+
+permute = list(range(N))
+random.shuffle(permute)
+inputs= inputs[permute,:]
+targets = targets[permute]
+
 # Task 09, Plotting Results (Provided)
+
+plt.plot([p[0] for p in classA] ,
+[p[1] for p in classA], 'b.')
+plt.plot([p[0] for p in classB] ,
+[p[1] for p in classB], 'r.')
+plt.axis('equal') # Force same s c a l e on both axes
+plt.savefig('svmplot.pdf') # Save a copy in a f i l e
+plt.show() # Show t h e p l o t on t h e s c r e e n
+
+xgrid = numpy.linspace(-5,5)
+ygrid = numpy.linspace(-4,4)
+
+grid = numpy.array([[indicator(x,y)
+for x in xgrid]
+for y in ygrid])
+plt.contour (xgrid,ygrid,grid,
+(-1.0,0.0,1.0),
+colors =('red','black','blue'),
+linewidths =(1,3,1))
+
 # Task 10, Exploring and Reporting
 # ------>  SubTask 01, Effects of Moving Clusters around and changing their sizes
 # ------>  SubTask 02, Effects of non-linear kernels
